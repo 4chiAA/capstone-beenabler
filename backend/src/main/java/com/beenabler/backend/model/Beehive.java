61b31f15ed746e816583af3c3355b43af35116fb
Achim Lenz <hello.achim@outlook.com>
2024-01-11 14:43:31 +0100

    add model beehive and enum beehive type

---
+++ backend/src/main/java/com/beenabler/backend/model/Beehive.java
@@ -0,0 +1,19 @@
+package com.beenabler.backend.model;
+import java.time.ZonedDateTime;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Beehive {
+    private String id;
+    private ZonedDateTime dateTime;
+    private String name;
+    private String location;
+    private BeehiveType type;
+}
