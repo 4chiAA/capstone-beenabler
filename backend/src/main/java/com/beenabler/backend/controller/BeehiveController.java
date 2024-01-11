c50f42473494212269575fe1146feacb538fd710
Achim Lenz <hello.achim@outlook.com>
2024-01-11 15:25:09 +0100

    add method getAllBeehives in service and matching GetMapping in controller

--- backend/src/main/java/com/beenabler/backend/controller/BeehiveController.java
+++ backend/src/main/java/com/beenabler/backend/controller/BeehiveController.java
@@ -1,9 +1,13 @@
 package com.beenabler.backend.controller;
 
+import com.beenabler.backend.model.Beehive;
 import com.beenabler.backend.service.BeehiveService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api")
@@ -11,4 +15,9 @@ import org.springframework.web.bind.annotation.RestController;
 public class BeehiveController {
 
     private final BeehiveService beehiveService;
+
+    @GetMapping("/beehives")
+    public List<Beehive> getAllBeehives(){
+        return beehiveService.getAllBeehives();
+    }
 }
--- backend/src/main/java/com/beenabler/backend/service/BeehiveService.java
+++ backend/src/main/java/com/beenabler/backend/service/BeehiveService.java
@@ -1,12 +1,19 @@
 package com.beenabler.backend.service;
 
+import com.beenabler.backend.model.Beehive;
 import com.beenabler.backend.repo.BeehiveRepo;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class BeehiveService {
 
     private final BeehiveRepo beehiveRepo;
+
+    public List<Beehive> getAllBeehives() {
+        return beehiveRepo.findAll();
+    }
 }
