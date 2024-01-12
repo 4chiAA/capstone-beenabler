608dd2414b903d11f4d124909dc763ae9137a3db
Achim Lenz <hello.achim@outlook.com>
2024-01-12 14:07:43 +0100

    add beehive type and preview component

---
+++ frontend/src/components/BeehivePreview.tsx
@@ -0,0 +1,27 @@
+import {Beehive} from "../types/Beehive.ts";
+
+type BeehivePreviewProps = {
+    beehives: Beehive[]
+}
+
+export default function BeehivePreview(props: BeehivePreviewProps){
+
+    if (props.beehives.length === 0) {
+        return ("You don't have any beehives")
+    }
+
+    return (
+        <>
+            <section>
+                {props.beehives.map((beehive: Beehive) => (
+                    <article key={beehive.id}>
+                        <p>{beehive.name}</p>
+                        <p>{beehive.dateTime}</p>
+                        <p>{beehive.type}</p>
+                    </article>
+                ))}
+            </section>
+        </>
+    )
+
+}
