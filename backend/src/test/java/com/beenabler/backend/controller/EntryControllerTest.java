05caa76d9b72371ac4f28733254c2c4525d32685
Achim Lenz <hello.achim@outlook.com>
2024-01-25 15:21:55 +0100

    remove unused imports

--- backend/src/test/java/com/beenabler/backend/controller/EntryControllerTest.java
+++ backend/src/test/java/com/beenabler/backend/controller/EntryControllerTest.java
@@ -4,7 +4,6 @@ import com.beenabler.backend.model.Beehive;
 import com.beenabler.backend.model.BeehiveDTO;
 import com.beenabler.backend.model.Entry;
 import com.beenabler.backend.model.EntryDTO;
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
